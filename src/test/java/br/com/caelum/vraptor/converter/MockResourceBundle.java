9a4b7621fb12abfb6074d1a46153b5326ec10362
Otávio Garcia <otavio@otavio.com.br>
2014-03-23 00:05:58 -0300

    Initial import

---
+++ src/test/java/br/com/caelum/vraptor/converter/MockResourceBundle.java
@@ -0,0 +1,19 @@
+package br.com.caelum.vraptor.converter;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+public class MockResourceBundle
+	extends ResourceBundle {
+
+	@Override
+	public Enumeration<String> getKeys() {
+		return Collections.emptyEnumeration();
+	}
+
+	@Override
+	protected Object handleGetObject(String key) {
+		return key;
+	}
+}
